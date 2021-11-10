27dcd3c05319d67412e811dfead0bcc084676ab9
Dennis-van-Gils <vangils.dennis@gmail.com>
2021-11-10 15:55:46 +0100

    Added comment on key

--- web/chemicals_delivery/globals.php
+++ web/chemicals_delivery/globals.php
@@ -9,6 +9,12 @@ define(__NAMESPACE__.'\WEB_ROOT', $protocol.$_SERVER['HTTP_HOST'].$_SERVER['CONT
 define(__NAMESPACE__.'\WEB_CHEMICALS_DELIVERY', \Globals\WEB_ROOT.'chemicals_delivery/');
 define(__NAMESPACE__.'\FILE_BUTTON_STATES', 'button_states.txt');
 
+// The Arduino can only successfully communicate with the web server if it sends
+// along the correct key. The key that is send by the Arduino is its MAC address
+// in normal ASCII form, like '84:CC:A8:62:1C:F2'. The server will internally
+// hash this key and compare this hash to the following string:
+//   Hashed MAC address of the Arduino.
+//   You can use tool https://phppasswordhash.com/
 define(__NAMESPACE__.'\HASHED_MAC_ADDRESS', '$2y$10$Mven4dAvVq6UYpSTUqZWFeoV.Tk9BgEFHCZxykEfcXQL6lMhtfRuy');
 
 // Email
