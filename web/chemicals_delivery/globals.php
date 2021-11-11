d80a684eefa1159886bd413ca89152d8e1b663c0
Dennis-van-Gils <vangils.dennis@gmail.com>
2021-11-11 16:26:24 +0100

    Added comments. Added email configuration.

--- web/chemicals_delivery/globals.php
+++ web/chemicals_delivery/globals.php
@@ -1,23 +1,33 @@
 <?php
+/**
+ * @author Dennis van Gils
+ * Web interface configuration to the Arduino WiFi `Chemicals Delivery` bell.
+ * Adjust the configuration to your needs.
+ */
 namespace Globals;
 
-// Web root, e.g.
-//   http://localhost/chemicals_delivery/   : Xampp local server (debug)
-//   https://xxx.tnw.utwente.nl/            : Internet server    (release)
+// Figure out the web root, e.g.
+//   http://localhost/chemicals_delivery/ : Xampp local server (debug)
+//   https://xxx.tnw.utwente.nl/          : Internet server    (release)
+// DO NOT MODIFY
 $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https://' : 'http://';
 define(__NAMESPACE__.'\WEB_ROOT', $protocol.$_SERVER['HTTP_HOST'].$_SERVER['CONTEXT_PREFIX'].'/');
+
+// URL of the web interface
 define(__NAMESPACE__.'\WEB_CHEMICALS_DELIVERY', \Globals\WEB_ROOT.'chemicals_delivery/');
+
+// Filename of the textfile on the server to store the button states to
 define(__NAMESPACE__.'\FILE_BUTTON_STATES', 'button_states.txt');
 
 // The Arduino can only successfully communicate with the web server if it sends
 // along the correct key. The key that is send by the Arduino is its MAC address
 // in normal ASCII form, like '84:CC:A8:62:1C:F2'. The server will internally
 // hash this key and compare this hash to the following string:
-//   Hashed MAC address of the Arduino.
-//   You can use tool https://phppasswordhash.com/
+//   You can use tool `https://phppasswordhash.com/` to transform the MAC
+//   address of the Arduino to its equivalent hash and paste it below.
 define(__NAMESPACE__.'\HASHED_MAC_ADDRESS', '$2y$10$Mven4dAvVq6UYpSTUqZWFeoV.Tk9BgEFHCZxykEfcXQL6lMhtfRuy');
 
 // Email
-// define(__NAMESPACE__.'\email_tag' , '[PoF Inv]');
-// define(__NAMESPACE__.'\email_name', 'PoF Inventory');
-// define(__NAMESPACE__.'\email_from', 'pof.inventory@'.gethostname());
+//   email_to: Comma-separated list of recipients
+define(__NAMESPACE__.'\email_to', 'd.p.m.vangils@utwente.nl');
+define(__NAMESPACE__.'\email_from', 'PoF Chemicals Delivery Bell <noreply@noreply.nl>');
