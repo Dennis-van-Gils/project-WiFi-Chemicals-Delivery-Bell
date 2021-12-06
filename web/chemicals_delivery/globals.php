e5cfd824dd41af874f9fb2b69f1bf0ca062e1267
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-12-06 21:09:44 +0100

    Update globals.php

--- web/chemicals_delivery/globals.php
+++ web/chemicals_delivery/globals.php
@@ -25,7 +25,7 @@ define(__NAMESPACE__.'\FILE_ARDUINO_STATUS', \Globals\DIR_ROOT.'_arduino_status.
 // The Arduino can only successfully communicate with the web server if it sends
 // along the correct key. The key that is send by the Arduino is its MAC address
 // in normal ASCII form, like '84:CC:A8:62:1C:F2'. The server will internally
-// hash this key and compare this hash to the following string:
+// hash this key and compare this hash to the string below:
 //   You can use tool `https://phppasswordhash.com/` to transform the MAC
 //   address of the Arduino to its equivalent hash and paste it below.
 define(__NAMESPACE__.'\HASHED_MAC_ADDRESS', '$2y$10$Mven4dAvVq6UYpSTUqZWFeoV.Tk9BgEFHCZxykEfcXQL6lMhtfRuy');
